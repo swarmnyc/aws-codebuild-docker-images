f9c4fcdfa70d8ae15c56d11a96d8e208a26a1fc0
Tao Ren <tour.ren.gz@gmail.com>
2017-12-08 18:02:12 -0500

    Push to Docker Hub instead of AWS ECR.

--- ubuntu/docker/1.13.1/build.sh
+++ ubuntu/docker/1.13.1/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-IMAGE_VERSION=1.13.7
-docker build -t aws-docker:${IMAGE_VERSION} .
-docker tag aws-docker:${IMAGE_VERSION} 195193742544.dkr.ecr.us-east-1.amazonaws.com/aws-docker:${IMAGE_VERSION}
-eval $( aws ecr get-login --no-include-email --region us-east-1 --profile eleven )
-docker push 195193742544.dkr.ecr.us-east-1.amazonaws.com/aws-docker:${IMAGE_VERSION}
+IMAGE_VERSION=1.13.1
+docker build -t swarmnyc/aws-docker:${IMAGE_VERSION} .
+docker push swarmnyc/aws-docker:${IMAGE_VERSION}
+# docker tag swarmnyc/aws-docker:${IMAGE_VERSION} 195193742544.dkr.ecr.us-east-1.amazonaws.com/aws-docker:${IMAGE_VERSION}
+# eval $( aws ecr get-login --no-include-email --region us-east-1 --profile eleven )
+# docker push 195193742544.dkr.ecr.us-east-1.amazonaws.com/aws-docker:${IMAGE_VERSION}
